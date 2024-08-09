c2ec69d495d3ad7eae8d8f15d9d39e253088974f
Kartik <kartik20044@gmail.com>
2024-08-09 05:45:06 -0300

    github and navbar adde

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 bg-background px-3 shodow-sm">
+      <nav className="mx-auto flex h-14 w-full max-w-7xl items-center justify-between gap-3">
+
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
