ba26132ebb5e98817e12c53dfb180bf3b116d622
Kartik <kartik20044@gmail.com>
2024-08-09 11:45:39 -0300

    github added or middle ware changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,38 @@
+import { auth, signIn } from "@/auth"
+import Link from "next/link"
+import { Button } from "./ui/button"
+import UserButton from "./UserButton"
 
-const Navbar = () => {
-  return (
-    <header className="sticky top-0 bg-background px-3 shodow-sm">
-      <nav className="mx-auto flex h-14 w-full max-w-7xl items-center justify-between gap-3">
-
-      </nav>
-    </header>
-  )
+const Navbar = async () => {
+    const session = await auth()
+    const user = session?.user
+    return (
+        <header className="sticky top-0 bg-background px-3 shadow-sm">
+            <nav className="mx-auto flex h-14 w-full max-w-7xl items-center justify-between gap-3">
+                <Link href={"/"} className=" font-bold">
+                    Next Auth V5
+                </Link>
+                {
+                    user ?
+                        <UserButton user={user} />
+                        : <SignInButton />
+                }
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
+
+function SignInButton() {
+    return (
+        <form action={async () => {
+            "use server";
+            await signIn()
+        }}>
+            <Button type="submit">
+                Sign In
+            </Button>
+        </form>
+    )
+}
